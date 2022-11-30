991f34d2f995386d2996962d38aa502d30e8c5b4
amirzulic <nokia5130>
2022-11-30 16:16:16 +0100

    Initial project skeleton using clean architecture

---
+++ ShowTask.Application/ShowService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowTask.Application
+{
+    public class ShowService : IShowService
+    {
+        private readonly IShowRepository _repository;
+    }
+}
---
+++ ShowTask.Infrastructure/ShowRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShowTask.Application;
+
+namespace ShowTask.Infrastructure
+{
+    public class ShowRepository : IShowRepository
+    {
+    }
+}
